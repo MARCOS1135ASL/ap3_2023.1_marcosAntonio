ea555086cef6775c29873dd5d906b49531b47075
MARCOS1135ASL <mlima3511@gmail.com>
2023-08-19 15:28:16 -0300

    Create OlaMundo.java

---
+++ projeto_ap3_marcosaAntonio/aula1/OlaMundo.java
@@ -0,0 +1,13 @@
+package projeto_ap3_marcosaAntonio.aula1;
+/* Escreva um programa em Java que imprima a mensagem "Olá, mundo!"
+ * no console. Implemente uma classe chamada "OlaMundo"
+ * no arquivo OlaMundo.java, com um método "main". Dentro do método "main",
+ * adicione o código necessário para imprimir a mensagem 
+ * "Olá, mundo!" usando a função System.out.println().
+ */
+public class OlaMundo {
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
+    }
+}
+
