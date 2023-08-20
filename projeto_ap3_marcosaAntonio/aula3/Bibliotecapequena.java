89caaee2bf38b0f3d7c54f6d9630bb4497c2f704
MARCOS1135ASL <mlima3511@gmail.com>
2023-08-20 09:59:35 -0300

    Create Bibliotecapequena.java

---
+++ projeto_ap3_marcosaAntonio/aula3/Bibliotecapequena.java
@@ -0,0 +1,7 @@
+package projeto_ap3_marcosaAntonio.aula3;
+//  Crie uma classe BibliotecaPequena com atributos nome (private) e livros[6] (protected). 
+//  A classe Biblioteca deve conter um método listarLivros de visibilidade public 
+//  que exibe os nomes dos livros cadastrados. Faça uma classe TestaBibliotecaPequena com o
+//  cadastro de 6 livros. Armazene o nome dos livros no array e use a classe Scanner para ler o nome de cada livro.
+
+public class BibliotecaPequena
