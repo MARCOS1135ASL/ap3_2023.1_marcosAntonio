674fce04fdb81fea1ea78e960eba1f3e43fcceab
MARCOS1135ASL <mlima3511@gmail.com>
2023-09-01 11:57:55 -0300

    Update Main.java

--- projeto_ap3_marcosaAntonio/unidade2_ex1/Main.java
+++ projeto_ap3_marcosaAntonio/unidade2_ex1/Main.java
@@ -1,4 +1,4 @@
-
+// agora iremos  cria a classe main para testa as funcionalidades do nosso programa 
 package projeto_ap3_marcosaAntonio.unidade2_ex1;
 
 public class Main {
@@ -7,12 +7,14 @@ public class Main {
         Disciplina disciplina3 = new Disciplina("AP3", 60);
         Disciplina disciplina4 = new Disciplina("Lógica", 60);
         Disciplina disciplina5 = new Disciplina("Banco de Dados", -100); // Carga horária negativa
+        Disciplina disciplina6 = new Disciplina(null, 0);
 
         Curso curso = new Curso("Sistemas de Informação");
 
         curso.adicionarDisciplina(disciplina3);
         curso.adicionarDisciplina(disciplina4);
         curso.adicionarDisciplina(disciplina5);
+        curso.adicionarDisciplina(disciplina6);
 
         System.out.println(curso.toString());
     }
