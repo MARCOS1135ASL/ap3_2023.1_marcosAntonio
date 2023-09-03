354c6350da8ca65b10828726755f648fd466fec3
MARCOS1135ASL <mlima3511@gmail.com>
2023-09-03 10:22:49 -0300

    Create SistemaUniversidades.java

---
+++ projeto_ap3_marcosaAntonio/unidade2_ex1/SistemaUniversidades.java
@@ -0,0 +1,13 @@
+package projeto_ap3_marcosaAntonio.unidade2_ex1;
+// Crie uma classe SistemaUniversidades que organize informações sobre várias universidades.
+// classe SistemaUniversidades deve conter uma lista de Universidade como parte de sua composição.
+//Adicione um método encontrarUniversidadeComMaisAlunos() que retorna o nome da universidade que possui mais alunos matriculados 
+// em todos os cursos. Utilize a classe Curso do exercício 1.
+
+import java.util.ArrayList;
+import java.util.List;
+ 
+public class SistemaUniversidades{
+    
+}
+
